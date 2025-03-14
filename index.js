421c33ddf80bbbccabae7ebc1bd3769853eb8236
duynguyentho <duynt2201@gmail.com>
2025-03-14 15:18:10 +0700

    test

--- index.js
+++ index.js
@@ -1,27 +1,49 @@
-const puppeteer = require("puppeteer");
+const { timeout } = require('puppeteer');
+const puppeteer = require('puppeteer-extra')
+const RecaptchaPlugin = require('puppeteer-extra-plugin-recaptcha')
+puppeteer.use(
+  RecaptchaPlugin({
+    provider: {
+      id: '2captcha',
+      token: ''
+    },
+    visualFeedback: true
+  })
+)
 
 async function scrapeGoogle(query) {
-    const browser = await puppeteer.launch({ headless: true });
+    const browser = await puppeteer.launch({ headless: false, executablePath: '/usr/bin/google-chrome' });
     const page = await browser.newPage();
     
     // Set user agent to mimic a real browser
     await page.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36");
     
     const searchUrl = `https://www.google.com/search?q=${encodeURIComponent(query)}`;
-    await page.goto(searchUrl, { waitUntil: "domcontentloaded" });
+    await page.goto(searchUrl, { timeout: 0 });
 
-    // Extract search results
+    await page.solveRecaptchas()
+    await page.waitForNavigation()
+
+    // for (let i = 0; i < 100; i++) {
+    //     await page.goto(`https://www.google.com/search?q=mua+laptop+${i}`, { waitUntil: "domcontentloaded" });
+    //     setTimeout(() => {
+    //         console.log('wait')
+    //     }, 1000)
+    // }
+    // // Extract search results
     const results = await page.evaluate(() => {
-        return Array.from(document.querySelectorAll("a")).map(el => ({
-                href: el.href,
-                text: el.textContent
+        return Array.from(document.querySelectorAll("a")).filter(el => (
+            el.href.includes("https://www.googleadservices.com/pagead")
+        )).map(el => ({
+            title: el.querySelector("h3") ? el.querySelector("h3").innerText : "",
+            url: el.href
         }));
     });
 
     console.log(results);
 
-    await browser.close();
+    // await browser.close();
 }
 
 // Run the function with a query
-scrapeGoogle("Node.js web scraping");
+scrapeGoogle("mua laptop");
